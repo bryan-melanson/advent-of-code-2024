8e2c965c49cd4605b4d3ab76e28331a2e3e8513e
Bryan Melanson <bryan@bryanmelanson.com>
2025-08-03 10:58:37 -0400

    Add Day 2 Pt2 Rust solution

--- rust/02/src/part2.rs
+++ rust/02/src/part2.rs
@@ -1,3 +1,55 @@
+fn row_to_vec(data: String) -> Vec<Vec<i32>> {
+    data.lines()
+        .map(|line| {
+            line.split_whitespace()
+                .map(|num| num.parse::<i32>().unwrap())
+                .collect()
+        })
+        .collect()
+}
+
+fn abs_diff_safe(a: i32, b: i32) -> bool {
+    (1..=3).contains(&a.abs_diff(b))
+}
+
+fn is_strict_and_safe(row: &[i32]) -> bool {
+    let mut direction = None;
+
+    for i in 1..row.len() {
+        if row[i] == row[i - 1] {
+            return false;
+        } else if direction.is_none() {
+            direction = Some(row[i] > row[i - 1]);
+        } else if direction.unwrap() != (row[i] > row[i - 1]) {
+            return false;
+        }
+
+        if !abs_diff_safe(row[i], row[i - 1]) {
+            return false;
+        }
+    }
+
+    true
+}
+
+fn is_row_safe_with_removal(row: &[i32]) -> bool {
+    if is_strict_and_safe(row) {
+        return true;
+    }
+
+    // Remove an element
+    for i in 0..row.len() {
+        let mut copy = Vec::with_capacity(row.len() - 1);
+        copy.extend_from_slice(&row[..i]);
+        copy.extend_from_slice(&row[i + 1..]);
+        if is_strict_and_safe(&copy) {
+            return true;
+        }
+    }
+
+    false
+}
+
 fn solution(path: &str) -> i32 {
     let path_obj = std::path::Path::new(path);
 
@@ -7,8 +59,16 @@ fn solution(path: &str) -> i32 {
     }
 
     // Import the data
-    let _data = std::fs::read_to_string(path_obj).expect("Failed to read file");
-    0
+    let data = std::fs::read_to_string(path_obj).expect("Failed to read file");
+    // Split rows into Vecs
+    let rows = row_to_vec(data);
+    // Check each row for safety
+    let count = rows
+        .iter()
+        .filter(|row| is_row_safe_with_removal(row))
+        .count() as i32;
+
+    count
 }
 
 pub fn print_solution(test_val: i32) {
